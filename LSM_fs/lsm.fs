e3b058a3ea89c443b73b9e452c90682e939ddc79
Eric Sink <eric@zumero.com>
2014-10-09 21:07:33 -0400

    fix names of byte compare funcs

--- LSM_fs/lsm.fs
+++ LSM_fs/lsm.fs
@@ -65,7 +65,7 @@ type IWrite =
 module ByteComparer = 
     // this code is very non-F#-ish.  but it's much faster.
 
-    let cmp (x:byte[], y:byte[]) =
+    let Compare (x:byte[], y:byte[]) =
         let xlen = x.Length
         let ylen = y.Length
         let len = if xlen<ylen then xlen else ylen
@@ -80,7 +80,7 @@ module ByteComparer =
                 i <- i + 1
         if i>len then result else (xlen - ylen)
 
-    let compareWithin (x:byte[],off,xlen,y:byte[]) =
+    let CompareWithin (x:byte[],off,xlen,y:byte[]) =
         let ylen = y.Length
         let len = if xlen<ylen then xlen else ylen
         let mutable i = 0
@@ -224,8 +224,8 @@ type private PageReader(pgsz:int) =
         cur <- cur + 1
         r
 
-    member this.cmp(len, other) =
-        ByteComparer.compareWithin(buf, cur, len, other)
+    member this.Compare(len, other) =
+        ByteComparer.CompareWithin(buf, cur, len, other)
 
     // page type could just be a property
     member this.PageType =
@@ -336,7 +336,7 @@ type MemorySegment() =
         // cannot be captured by a closure.
         let cur = ref -1
         let keys:byte[][] = (Array.ofSeq pairs.Keys)
-        let sortfunc x y = ByteComparer.cmp(x, y)
+        let sortfunc x y = ByteComparer.Compare(x, y)
         Array.sortInPlaceWith sortfunc keys
 
         let rec search k min max sop le ge = 
@@ -347,7 +347,7 @@ type MemorySegment() =
             else
                 let mid = (max + min) / 2
                 let kmid = keys.[mid]
-                let cmp = ByteComparer.cmp (kmid, k)
+                let cmp = ByteComparer.Compare (kmid, k)
                 if 0 = cmp then mid
                 else if cmp<0  then search k (mid+1) max sop mid ge
                 else search k min (mid-1) sop le mid
@@ -372,7 +372,7 @@ type MemorySegment() =
                 keys.[!cur]
             
             member this.KeyCompare(k) =
-                ByteComparer.cmp(keys.[!cur], k)
+                ByteComparer.Compare(keys.[!cur], k)
 
             member this.Value() =
                 let v = pairs.[keys.[!cur]]
@@ -879,11 +879,11 @@ module BTreeSegment =
             let kflag = pr.GetByte()
             let klen = pr.GetVarint() |> int
             if 0uy = (kflag &&& FLAG_OVERFLOW) then
-                pr.cmp(klen, other)
+                pr.Compare(klen, other)
             else
                 let pagenum = pr.ReadUInt32()
                 let k = readOverflow klen fs pagenum
-                ByteComparer.cmp (k, other)
+                ByteComparer.Compare (k, other)
 
         let keyInLeaf n = 
             pr.SetPosition(leafKeys.[n])
@@ -982,7 +982,7 @@ module BTreeSegment =
             ok
 
         let rec searchInParentPage k (ptrs:uint32[]) (keys:byte[][]) (i:uint32) :uint32 =
-            let cmp = ByteComparer.cmp (k, keys.[int i])
+            let cmp = ByteComparer.Compare (k, keys.[int i])
             if cmp>0 then
                 searchInParentPage k ptrs keys (i+1u)
             else
