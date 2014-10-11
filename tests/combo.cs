340862d5feaa43b6a0fc31ff3cab7c3dc83a5f16
Eric Sink <eric@zumero.com>
2014-10-11 07:37:05 -0500

    organized tests project a little better

---
+++ tests/combo.cs
@@ -0,0 +1,91 @@
+﻿/*
+	Copyright 2014 Zumero, LLC
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	    http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+//using Xunit;
+
+using Zumero.LSM;
+
+namespace lsm_tests
+{
+	public class combo
+	{
+		public Func<IWrite> create_memory_segment;
+		public Func<Stream,ICursor,uint> create_btree_segment;
+		public Func<Stream,long,ICursor> open_btree_segment;
+		public Func<ICursor,ICursor> create_living_cursor;
+		public Func<ICursor,ICursor,ICursor> create_multicursor;
+
+		public static combo make_cs()
+		{
+			combo c = new combo ();
+			c.create_memory_segment = Zumero.LSM.cs.MemorySegment.Create;
+			c.create_btree_segment = Zumero.LSM.cs.BTreeSegment.Create;
+			c.open_btree_segment = Zumero.LSM.cs.BTreeSegment.OpenCursor;
+			c.create_living_cursor = (ICursor csr) => new Zumero.LSM.cs.LivingCursor(csr);
+			c.create_multicursor = (ICursor a, ICursor b) => Zumero.LSM.cs.MultiCursor.create (a, b);
+			return c;
+		}
+
+		public static combo make_fs()
+		{
+			combo c = new combo ();
+			c.create_memory_segment = Zumero.LSM.fs.MemorySegment.Create;
+			c.create_btree_segment = Zumero.LSM.fs.BTreeSegment.Create;
+			c.open_btree_segment = Zumero.LSM.fs.BTreeSegment.OpenCursor;
+			c.create_living_cursor = (ICursor csr) => new Zumero.LSM.fs.LivingCursor(csr);
+			c.create_multicursor = (ICursor a, ICursor b) => Zumero.LSM.fs.MultiCursor.create (a, b);
+			return c;
+		}
+
+		public static combo make_cs_fs()
+		{
+			combo c = new combo ();
+			c.create_memory_segment = Zumero.LSM.cs.MemorySegment.Create;
+			c.create_btree_segment = Zumero.LSM.cs.BTreeSegment.Create;
+			c.open_btree_segment = Zumero.LSM.fs.BTreeSegment.OpenCursor;
+			c.create_living_cursor = (ICursor csr) => new Zumero.LSM.fs.LivingCursor(csr);
+			c.create_multicursor = (ICursor a, ICursor b) => Zumero.LSM.fs.MultiCursor.create (a, b);
+			return c;
+		}
+
+		public static combo make_fs_cs()
+		{
+			combo c = new combo ();
+			c.create_memory_segment = Zumero.LSM.fs.MemorySegment.Create;
+			c.create_btree_segment = Zumero.LSM.fs.BTreeSegment.Create;
+			c.open_btree_segment = Zumero.LSM.cs.BTreeSegment.OpenCursor;
+			c.create_living_cursor = (ICursor csr) => new Zumero.LSM.cs.LivingCursor(csr);
+			c.create_multicursor = (ICursor a, ICursor b) => Zumero.LSM.cs.MultiCursor.create (a, b);
+			return c;
+		}
+
+		public static List<combo> get_combos()
+		{
+			List<combo> a = new List<combo>();
+			a.Add(combo.make_cs());
+			a.Add(combo.make_fs());
+			a.Add(combo.make_cs_fs());
+			a.Add(combo.make_fs_cs());
+			return a;
+		}
+	}
+
+}
+
---
+++ tests/ex.cs
@@ -0,0 +1,73 @@
+﻿/*
+	Copyright 2014 Zumero, LLC
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	    http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+//using Xunit;
+
+using Zumero.LSM;
+
+namespace lsm_tests
+{
+	public static class hack
+	{
+		public static string from_utf8(this Stream s)
+		{
+			// note the arbitrary choice of getting this function from cs instead of fs
+			// maybe utils should move into LSM_base
+			return Zumero.LSM.cs.utils.ReadAll (s).FromUTF8 ();
+		}
+
+		public static void Insert(this IWrite w, byte[] k, byte[] v)
+		{
+			w.Insert (k, new MemoryStream(v) );
+		}
+
+		public static void Insert(this IWrite w, string k, byte[] v)
+		{
+			w.Insert (k.ToUTF8 (), new MemoryStream(v) );
+		}
+
+		public static void Insert(this IWrite w, string k, string v)
+		{
+			w.Insert (k.ToUTF8 (), new MemoryStream(v.ToUTF8 ()) );
+		}
+
+		public static void Delete(this IWrite w, string k)
+		{
+			w.Delete (k.ToUTF8 ());
+		}
+
+		public static void Seek(this ICursor csr, string k, SeekOp sop)
+		{
+			csr.Seek (k.ToUTF8(), sop);
+		}
+
+		public static byte[] ToUTF8(this string s)
+		{
+			return System.Text.Encoding.UTF8.GetBytes (s);
+		}
+
+		public static string FromUTF8(this byte[] ba)
+		{
+			return System.Text.Encoding.UTF8.GetString (ba, 0, ba.Length);
+		}
+	}
+
+}
+
