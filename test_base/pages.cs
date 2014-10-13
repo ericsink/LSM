f538b808611dde8f90ccd010bc3556919bc51eaf
Eric Sink <eric@zumero.com>
2014-10-13 05:18:23 -0500

    moved the threads test into the tests project.  currently still only runs on the cs combo.

--- test_base/pages.cs
+++ test_base/pages.cs
@@ -1,4 +1,19 @@
-﻿
+﻿/*
+	Copyright 2014 Zumero, LLC
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	    http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -34,8 +49,7 @@ namespace lsm_tests
 		void IPages.End(string token, int lastPage)
 		{
 			var blocks = segments [token];
-			Console.WriteLine ("{0} is done", token);
-			//Console.WriteLine ("{0}", blocks);
+			//Console.WriteLine ("{0} is done", token);
 		}
 
 		Tuple<int,int> IPages.GetRange(string token)
@@ -44,7 +58,7 @@ namespace lsm_tests
 				var t = new Tuple<int,int> (cur, cur + 9);
 				cur = cur + 13;
 				segments [token].Add (t);
-				Console.WriteLine ("{0} gets {1} --> {2}", token, t.Item1, t.Item2);
+				//Console.WriteLine ("{0} gets {1} --> {2}", token, t.Item1, t.Item2);
 				return t;
 			}
 		}
