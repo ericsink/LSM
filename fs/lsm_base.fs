847931b9586dd9423f13ad8ebce922a842c7b15a
Eric Sink <eric@zumero.com>
2014-10-23 12:39:56 -0500

    lsm base in fs.  not used yet.

---
+++ fs/lsm_base.fs
@@ -0,0 +1,41 @@
+﻿(*
+    Copyright 2014 Zumero, LLC
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*)
+
+namespace Zumero.LSM
+
+open System
+open System.IO
+
+type IPages =
+    abstract member PageSize : int with get
+    abstract member Begin : unit->Guid
+    abstract member GetRange : Guid->int*int
+    abstract member End : Guid->int->unit
+
+type SeekOp = SEEK_EQ=0 | SEEK_LE=1 | SEEK_GE=2
+
+type ICursor =
+    abstract member Seek : k:byte[] * sop:SeekOp -> unit
+    abstract member First : unit -> unit
+    abstract member Last : unit -> unit
+    abstract member Next : unit -> unit
+    abstract member Prev : unit -> unit
+    abstract member IsValid : unit -> bool
+    abstract member Key : unit -> byte[]
+    abstract member Value : unit -> Stream
+    abstract member ValueLength : unit -> int
+    abstract member KeyCompare : k:byte[] -> int
+
