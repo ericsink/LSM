8d0c13985a595cc373cbd9917af0334fa3c26ec7
Eric Sink <eric@zumero.com>
2014-12-13 12:47:23 -0600

    put base.fs into lsm.fs and eliminate the extra proj file

--- fs/lsm.fs
+++ fs/lsm.fs
@@ -22,9 +22,83 @@ open System.Linq // used for OrderBy
 
 open FSharpx.Collections
 
-open Zumero.LSM
-
 type kvp = System.Collections.Generic.KeyValuePair<byte[],Stream>
+
+type IPendingSegment = interface end
+
+// TODO PageBlock, struct vs. record
+// struct is a little faster and more compatible with C#
+// record is more F# idiomatic
+type PageBlock = 
+    struct
+        val firstPage: int32
+        val lastPage: int32
+        new (f,l) = { firstPage = f; lastPage = l; }
+        override this.ToString() = sprintf "(%d,%d)" this.firstPage this.lastPage
+        member this.CountPages with get() = this.lastPage - this.firstPage + 1
+    end
+
+
+type IPages =
+    abstract member PageSize : int with get
+    abstract member Begin : unit->IPendingSegment
+    abstract member GetBlock : IPendingSegment->PageBlock
+    abstract member End : IPendingSegment*int->Guid
+
+type SeekOp = SEEK_EQ=0 | SEEK_LE=1 | SEEK_GE=2
+
+type ICursor =
+    inherit IDisposable
+    abstract member Seek : k:byte[] * sop:SeekOp -> unit
+    abstract member First : unit -> unit
+    abstract member Last : unit -> unit
+    abstract member Next : unit -> unit
+    abstract member Prev : unit -> unit
+    // the following are methods instead of properties because
+    // ICursor doesn't know how expensive they are to implement.
+    abstract member IsValid : unit -> bool
+    abstract member Key : unit -> byte[]
+    abstract member Value : unit -> Stream
+    abstract member ValueLength : unit -> int
+
+    abstract member KeyCompare : k:byte[] -> int
+
+type IWriteLock =
+    inherit IDisposable
+    abstract member CommitSegments : seq<Guid> -> unit
+    abstract member CommitMerge : Guid -> unit
+
+type IDatabaseFile =
+    abstract member OpenForReading : unit -> Stream
+    abstract member OpenForWriting : unit -> Stream
+
+type IDatabase = 
+    inherit IDisposable
+    abstract member WriteSegmentFromSortedSequence : seq<kvp> -> Guid
+    abstract member WriteSegment : System.Collections.Generic.IDictionary<byte[],Stream> -> Guid
+
+    abstract member OpenCursor : unit->ICursor 
+    // TODO consider name such as OpenLivingCursorOnCurrentState()
+    // TODO consider OpenCursorOnSegmentsInWaiting(seq<Guid>)
+    // TODO consider ListSegmentsInCurrentState()
+    // TODO consider OpenCursorOnSpecificSegment(seq<Guid>)
+
+    // TODO consider NeverMind(seq<Guid>) which explicitly 
+    // frees segments in waiting.
+
+    abstract member RequestWriteLock : unit->Async<IWriteLock>
+
+    abstract member Merge : int*int*bool*bool -> Async<Guid list> option
+    abstract member BackgroundMergeJobs : unit->Async<Guid list> list
+
+    // TODO maybe allow settings only when the object is constructed.
+    abstract member AutoMerge : bool with get, set
+
+module CursorUtils =
+    let ToSortedSequenceOfKeyValuePairs (csr:ICursor) = 
+        seq { csr.First(); while csr.IsValid() do yield new kvp(csr.Key(), csr.Value()); csr.Next(); done }
+    let ToSortedSequenceOfTuples (csr:ICursor) = 
+        seq { csr.First(); while csr.IsValid() do yield (csr.Key(), csr.Value()); csr.Next(); done }
 
 type pgitem = 
     struct
