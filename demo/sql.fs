abaeb390923b22099f56167917b4fe408bf5ecc6
Eric Sink <eric@zumero.com>
2015-02-09 15:52:03 -0600

    still fiddling with fparsec

--- demo/sql.fs
+++ demo/sql.fs
@@ -23,4 +23,39 @@ module SqlStuff =
     open System.Text
     open FParsec
 
+    type UserState = unit // doesn't have to be unit, of course
+    type Parser<'t> = Parser<'t, UserState>
+
+    let unescaped_identifier :Parser<_> =
+        let isIdentifierFirstChar c = isLetter c || c = '_'
+        let isIdentifierChar c = isLetter c || isDigit c || c = '_' || c = '$'
+        many1Satisfy2L isIdentifierFirstChar isIdentifierChar "unescaped_identifier"
+
+    // TODO escape sequences inside
+    let escaped_identifier :Parser<_> = 
+        let normalChar = satisfy (fun c -> c <> '`')
+        let escapedChar = stringReturn "``" '`'
+        between (pstring "`") (pstring "`") (manyChars (normalChar <|> escapedChar))
+ 
+    let stringLiteral :Parser<_> =
+        let escape =  anyOf "\"\\/bfnrt"
+                      |>> function
+                          | 'b' -> "\b"
+                          | 'f' -> "\u000C"
+                          | 'n' -> "\n"
+                          | 'r' -> "\r"
+                          | 't' -> "\t"
+                          | c   -> string c // every other char is mapped to itself
+
+        let unicodeEscape =
+            pstring "u" >>. pipe4 hex hex hex hex (fun h3 h2 h1 h0 ->
+                let hex2int c = (int c &&& 15) + (int c >>> 6)*9 // hex char to int
+                (hex2int h3)*4096 + (hex2int h2)*256 + (hex2int h1)*16 + hex2int h0
+                |> char |> string
+            )
+
+        between (pstring "\"") (pstring "\"")
+                (stringsSepBy (manySatisfy (fun c -> c <> '"' && c <> '\\')) (pstring "\\" >>. (escape <|> unicodeEscape)))
+                              
+                             
 
