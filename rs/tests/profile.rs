99075d45527328d304c139db0ccd72226ea569aa
Eric Sink <eric@zumero.com>
2015-05-21 18:18:23 -0600

    fiddling with performance issues and optimization.
    I moved a copy of the bunch test into the tests directory,
    changed it from a bench to a test, and marked it ignored.
    I wanted to be able to run it the way I used to, by itself,
    with 100 thousand as the basic size.
    
    I found the stuff in the Cargo.toml manifest docs to configure
    optimization profile.  I configured profile.test to build with
    opt-level 3.
    
    I ran the test, and it was really slow, as if the optimizer
    wasn't doing anything at all.
    
    So I realized the main difference here is that the test is
    no longer in the same crate as the library itself.
    
    I moved another copy of the test back into the same crate as
    the library and compiled it with optimization.  Now it's back
    to being around 15x as fast.
    
    Proceeding with the conjecture that this is all about cross-crate
    inlining, I went digging and found this:
    
    https://internals.rust-lang.org/t/when-should-i-use-inline/598/2
    
    So I changed the profile.test to compile with lto, following the
    idea that this would basically allow anything to be inlined at
    link time whether I put inline attributes on my functions or
    not.
    
    Rebuild, run the test, and now it crashes with a seg fault.
    
    Still digging...

---
+++ rs/tests/profile.rs
@@ -0,0 +1,56 @@
+
+extern crate lsm;
+
+fn tid() -> String {
+    // TODO use the rand crate
+    fn bytes() -> std::io::Result<[u8;16]> {
+        use std::fs::OpenOptions;
+        let mut f = try!(OpenOptions::new()
+                .read(true)
+                .open("/dev/urandom"));
+        let mut ba = [0;16];
+        try!(lsm::utils::ReadFully(&mut f, &mut ba));
+        Ok(ba)
+    }
+
+    fn to_hex_string(ba: &[u8]) -> String {
+        let strs: Vec<String> = ba.iter()
+            .map(|b| format!("{:02X}", b))
+            .collect();
+        strs.connect("")
+    }
+
+    let ba = bytes().unwrap();
+    to_hex_string(&ba)
+}
+
+fn tempfile(base: &str) -> String {
+    std::fs::create_dir("tmp");
+    let file = "tmp/".to_string() + base + "_" + &tid();
+    file
+}
+
+#[test]
+#[ignore]
+fn profile() {
+    fn f() -> std::io::Result<bool> {
+        //println!("running");
+        let mut db = try!(lsm::Database::db::new(&tempfile("profile"), lsm::DefaultSettings));
+
+        const NUM : usize = 100000;
+
+        let mut a = Vec::new();
+        for i in 0 .. 10 {
+            let g = try!(db.WriteSegmentFromSortedSequence(lsm::GenerateNumbers {cur: i * NUM, end: (i+1) * NUM, step: i+1}));
+            a.push(g);
+        }
+        try!(db.commitSegments(a.clone()));
+        let g3 = try!(db.merge(a));
+        try!(db.commitMerge(g3));
+
+        let res : std::io::Result<bool> = Ok(true);
+        res
+    }
+    assert!(f().is_ok());
+}
+
